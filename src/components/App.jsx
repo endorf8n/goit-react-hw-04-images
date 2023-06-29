68dfee7da14aa69955c06927b70ded7ddeecd061
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-06-29 15:37:17 +0300

    if(!q) return add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,7 @@ export const App = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    if (!q) return;
     const fetchData = () => {
       try {
         getImages(q, page).then(({ hits, totalHits }) => {
@@ -62,76 +63,3 @@ export const App = () => {
     </AppWrapper>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     page: 1,
-//     q: '',
-//     images: [],
-//     showBtn: false,
-//     loading: false,
-//   };
-
-// componentDidUpdate(_, prevState) {
-//     const { q, page } = this.state;
-
-//     if (q !== prevState.q || page !== prevState.page) {
-//       this.fetchData();
-//       }
-//   }
-
-//   fetchData = async () => {
-//     const { q, page } = this.state;
-// try {
-//   await getImages(q, page).then(({ hits, totalHits }) => {
-//     this.setState(prevState => ({
-//       images: [...prevState.images, ...hits],
-//       showBtn: page < Math.ceil(totalHits / 12),
-//     }));
-//     if (hits.length === 0) {
-//       return toast.info(
-//         'Sorry, there are no images matching your search query. Please try again'
-//       );
-//     }
-//   });
-// } catch (error) {
-//   toast.error('Something went wrong. Please, try later.');
-// } finally {
-//   this.setState({ loading: false });
-// }
-//   };
-
-// onSubmit = q => {
-//   if (q.trim() === '') {
-//     return toast.info(
-//       'Sorry, but the search field cannot be empty, please enter your query'
-//     );
-//   }
-//   this.setState({
-//     q,
-//     page: 1,
-//     images: [],
-//     showBtn: false,
-//     loading: true,
-//   });
-// };
-
-// onBtnMoreClick = () => {
-//   this.setState(prevState => ({
-//     page: prevState.page + 1,
-//   }));
-// };
-
-//   render() {
-//     const { images, showBtn, loading } = this.state;
-
-// return (
-//   <AppWrapper>
-//     <Searchbar onSubmit={this.onSubmit} />
-//     {loading ? <Loader /> : <ImageGallery images={images} />}
-
-//     {showBtn && <Button onBtnMoreClick={this.onBtnMoreClick} />}
-//   </AppWrapper>
-// );
-//   }
-// }
